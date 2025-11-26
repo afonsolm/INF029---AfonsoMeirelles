a00da1ccd07117e56815778f35e6e067c21f67fd
afonsolm <afonsolm03@gmail.com>
2025-11-26 12:41:08 -0300

    Update AfonsoMeirelles20251160022.c

--- trabalho1/AfonsoMeirelles20251160022.c
+++ trabalho1/AfonsoMeirelles20251160022.c
@@ -21,7 +21,7 @@
 // #################################################
 
 #include <stdio.h>
-#include "trabalho1.h" 
+#include "AfonsoMeirelles20251160022.h" 
 #include <stdlib.h>
 
 DataQuebrada quebraData(char data[]);
